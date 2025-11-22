783afd9cbc933d7c9c94015ac129f687549701bd
infonkadm-spec <infonk.adm@gmail.com>
2025-11-22 07:58:38 -0300

    alterado codigo pixel

--- components/header-script.tsx
+++ components/header-script.tsx
@@ -1,16 +1,6 @@
 import Script from "next/script";
 
-const idList: Record<string, string> = {
-  kim: "6920f79745ab396fb596faa4",
-  elon: "6920f79745ab396fb596faa4",
-  shakira: "6920f79745ab396fb596faa4",
-};
-
 export default function HeaderScript({ content }: { content: string }) {
-
-  const pixelId = idList[content];
-
-  if (!pixelId) return null;
 
   return (
     <>
@@ -18,19 +8,15 @@ export default function HeaderScript({ content }: { content: string }) {
         id="utmify-config"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `window.pixelId=${JSON.stringify(pixelId)};`,
+          __html: `window.pixelId = "6920f79745ab396fb596faa4";`,
         }}
       />
       <Script
         id="utmify-pixel"
         src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
         strategy="afterInteractive"
-      />
-      <Script
-        id="utmify-utms"
-        src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-        data-utmify-prevent-subids
-        strategy="afterInteractive"
+        async
+        defer
       />
     </>
   );
