0246dbaa8fb8e5a13778941c8982ac2d978acc96
infonkadm-spec <infonk.adm@gmail.com>
2025-12-01 01:34:56 -0300

    Update Megan page button timing to 10 minutes and 30 seconds

--- components/pages/black/megan/page5.tsx
+++ components/pages/black/megan/page5.tsx
@@ -29,7 +29,8 @@ export default function Page({
   // Deve bater com o ID do player VTurb (sem o prefixo "vid-")
   const videoId = "6925e0122126a6cf9409a026";
   const backLink = `https://${userHost}/promo`;
-  const pitchTime = 600;
+  // 10:30 = 630 segundos
+  const pitchTime = 630;
   const ctaLink = `${userFrontLink}${userFrontLink.includes("?") ? "&" : "?"}src=megan`;
 
   // VIDEO VERIFY
