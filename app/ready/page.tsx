a9b45ec64e855445e7992f980f513180e56ef8c9
infonkadm-spec <infonk.adm@gmail.com>
2025-12-05 02:09:07 -0300

    atualizacao pagina upsell

--- app/ready/page.tsx
+++ app/ready/page.tsx
@@ -8,20 +8,21 @@ export default function Page() {
       <div className="w-full text-sm bg-red-600 text-white">
         <div className="mx-auto px-4 py-4">
           <div className="text-center font-semibold">
-            Please do not close or refresh this page, as there may be errors when paying the fee!
+          Please do not close or refresh this page, as errors may occur when paying the fee!
           </div>
         </div>
       </div>
       <div className="flex flex-col w-full max-w-xl gap-6 px-5 py-6">
         <div className="flex flex-col items-center gap-2.5 text-center">
           <span className="text-xs italic opacity-60">
-            We are processing your payment…
+          We are processing your fee payment…
           </span>
           <Progress progress={96} />
         </div>
         <div className="flex flex-col text-center text-sm rounded-3xl gap-5 bg-gradient-to-t appear border-t px-5 py-6 -mb-5 from-gray-100 to-white border-gray-300">
           <div className="font-bold">
-            Ok, we&apos;ll give you one more chance, your last chance on this page only!
+          🎉 Congratulations! You won a surprise! 🎁
+          Click the green button below and receive it immediately!
           </div>
           <HotmartUpsell black={true} />
         </div>
